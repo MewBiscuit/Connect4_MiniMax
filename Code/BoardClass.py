864b5e1dc5150b7e71a1b2fe1993b10a4c02d8d7
MewBiscuit <91554425+MewBiscuit@users.noreply.github.com>
2021-11-15 23:24:58 +0100

    Delete Code directory

--- Code/BoardClass.py
+++
@@ -1,9 +0,0 @@
-class BoardClass:
-    def __init__(self, rows, columns):
-        self.rows = rows  # We save the number of rows
-        self.columns = columns  # and columns, will be useful later
-        self.slots = [[0] * columns for _ in range(rows)]  # Initialize all slots as empty
-
-    def __print__(self):  # Simple method to print the board without writing a loop each time
-        for slot in self.slots:
-            print(slot)
